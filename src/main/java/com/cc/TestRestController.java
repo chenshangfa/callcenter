a916e70be242d9cb1613a6d29c3c69a9ffa8b0d0
chenshangfa <15950589257@139.com>
2016-07-06 16:10:14 +0800

    Signed-off-by: chenshangfa <15950589257@139.com>

--- src/main/java/com/cc/TestRestController.java
+++ src/main/java/com/cc/TestRestController.java
@@ -5,14 +5,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TestRestController {
-	//接收get请求
-    @RequestMapping(value="/test")
-    @ResponseBody
-    public void  test(String username,HttpServletRequest request,HttpServletResponse response) throws Exception{
-    	response.getWriter().write("spring mvc is hello");
+    @RequestMapping(value="/main")
+    public String  test(String username,HttpServletRequest request,HttpServletResponse response) throws Exception{
+    	return "/main/main";
     }
 }
