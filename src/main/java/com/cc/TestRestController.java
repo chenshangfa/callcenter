bf5503ab61b3dda1e1c4f2b1742bb5cfc583c365
chenshangfa <15950589257@139.com>
2016-07-08 10:29:09 +0800

    commit update see

--- src/main/java/com/cc/TestRestController.java
+++ src/main/java/com/cc/TestRestController.java
@@ -28,7 +28,7 @@ public class TestRestController {
     }
     @RequestMapping(value="/see")
     public String  see(String username,HttpServletRequest request,HttpServletResponse response) throws Exception{
-    	return "/main/login.jsp";
+    	return "/main/see.jsp";
     }
     
    
